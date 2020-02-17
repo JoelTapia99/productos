3a9e2ab6c41d49840e8602e8074da0b3abfaa125
JoelTapia99 <joelts159@gmail.com>
2020-02-16 21:50:51 -0500

    proyecto en nest.js

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from "@nestjs/mongoose";
+import { ProductoModule } from './producto/producto.module';
+
+
+@Module({
+  imports: [ MongooseModule.forRoot('mongodb://localhost/nest'),
+   ProductoModule ],
+  controllers: [AppController,
+  ],
+  providers: [AppService
+  ],
+})
+export class AppModule {}
